dc76569041f26c3351fa98e5c6e3a44b9c2a9dcf
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-06-20 21:55:51 +0800

    read data from 591

--- Easy-Rent/read.js
+++ Easy-Rent/read.js
@@ -1,1 +1,6 @@
-alert('hello world')
+console.log('Start loading data from 591')
+$.get('https://rent.591.com.tw/index.php?module=search&action=rslist&is_new_list=1&type=1&searchtype=1&region=1&rentprice=3&area=1&kind=2&option=cold,icebox,hotwater,washer,bed,wardrobe&other=balcony_1,cook&order=area&orderType=desc', function(response){
+	console.log('Get data from 591');
+	$('#container .body').html(response.main);
+});
+
