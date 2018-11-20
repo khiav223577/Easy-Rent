413d1a4caae5ae584f0111a303a9bd5610e21b6a
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-11-20 14:50:06 +0800

    remove dead code

--- Easy-Rent/read.js
+++ Easy-Rent/read.js
@@ -19,13 +19,12 @@ function genSearchPath(kind, row){
     other: 'cook',
     firstRow: row,
   }
+  // var searchPath = path_591 + '/index.php?module=search&action=rslist&is_new_list=1&type=1&searchtype=1&region=1&listview=img&option=cold,hotwater,bed,wardrobe&kind=2&rentprice=3&order=area&orderType=desc&other=cook';
+  // var searchPath = path_591 + 'index.php?module=search&action=rslist&is_new_list=1&type=1&searchtype=1&region=1&rentprice=3&area=1&kind=2&option=cold,icebox,hotwater,washer,bed,wardrobe&other=balcony_1,cook&order=area&orderType=desc';
   return path_591 + 'index.php?' + Object.keys(params).map(k => k + '=' + params[k]).join('&');;
 }
 
-//var searchPath = path_591 + '/index.php?module=search&action=rslist&is_new_list=1&type=1&searchtype=1&region=1&listview=img&option=cold,hotwater,bed,wardrobe&kind=2&rentprice=3&order=area&orderType=desc&other=cook';
-// var searchPath = path_591 + 'index.php?module=search&action=rslist&is_new_list=1&type=1&searchtype=1&region=1&rentprice=3&area=1&kind=2&option=cold,icebox,hotwater,washer,bed,wardrobe&other=balcony_1,cook&order=area&orderType=desc';
 var row = 0;
-var searchPath = path_591 + 'index.php?module=search&action=rslist&is_new_list=1&type=1&searchtype=1&region=1&kind=2&rentprice=3&listview=img&order=area&orderType=desc&firstRow=';
 $('#container .body_591').text('loading...');
 $('#container .tabs .btn-success').click(function(){
   if (loadingFlag) return;
